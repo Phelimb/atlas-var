66b197ec1d95699cd84cf557813e01e95feb0e20
Phelim Bradley <wave@phel.im>
2016-03-16 15:31:44 +0000

    .

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
-from pip.req import parse_requirements
-import ga4ghmongo.version
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='ga4ghmongo',
-    version=ga4ghmongo.version.__version__,
+    version='0.0.1.1',
     packages=[
         'ga4ghmongo',
         'ga4ghmongo.schema',
         'ga4ghmongo.schema.models'],
     license='MIT',
     url='https://github.com/Phelimb/ga4gh-mongo',
-    description='.',
+    description='A document based Variant database inspired by ga4gh Variants schema',
     author='Phelim Bradley',
     author_email='wave@phel.im',
-    install_requires=reqs)
+    install_requires=["mongoengine"])
