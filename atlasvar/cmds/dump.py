5f83a6cf150b9af194c06ae5e930ff2960895c82
Phelim Bradley <wave@phel.im>
2017-02-03 13:36:14 +0000

    Bug fixing in probe set generation

--- atlasvar/cmds/dump.py
+++ atlasvar/cmds/dump.py
@@ -5,6 +5,8 @@ import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
 import datetime
 import logging
+logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,6 +15,7 @@ from mongoengine import connect
 from ga4ghmongo.schema import Variant
 from atlasvar.probes import AlleleGenerator
 from atlasvar.probes import make_variant_probe
+import math
 
 
 def get_non_singelton_variants(db_name):
@@ -33,21 +36,27 @@ def run(parser, args):
     if args.verbose:
         logger.setLevel(level=logging.DEBUG)
     else:
-        logger.setLevel(level=logging.ERROR)
+        logger.setLevel(level=logging.INFO)
     al = AlleleGenerator(
         reference_filepath=args.reference_filepath,
         kmer=args.kmer)
     _variant_ids = get_non_singelton_variants(db_name)
-    for variant in Variant.snps(id__in=_variant_ids).order_by("start"):
-    #for variant in Variant.snps().order_by("start"):
-        variant_panel = make_variant_probe(al, variant, args.kmer, DB=DB)
-        for i, ref in enumerate(variant_panel.refs):
-            sys.stdout.write(
-                ">ref-%s?var_name=%snum_alts=%i&ref=%s&enum=%i\n" %
-                (variant_panel.variant.var_hash, variant.var_name[:100], len(
-                    variant_panel.alts), variant_panel.variant.reference.id, i))
-            sys.stdout.write("%s\n" % ref)
-        for i, a in enumerate(variant_panel.alts):
-            sys.stdout.write(">alt-%s?var_name=%s&enum=%i\n" %
-                             (variant_panel.variant.var_hash, variant.var_name[:100], i))
-            sys.stdout.write("%s\n" % a)
+    total = Variant.snps(id__in=_variant_ids).count()
+    N = 100
+    pages = math.ceil(total / N)
+    for page in range(pages):
+        logger.info("%i of %i - %f%%" %
+                    (page*N, total, round(100*(page*N)/total, 2)))
+        for variant in Variant.snps(id__in=_variant_ids).order_by("start").skip(N*page).limit(N):
+            # for variant in Variant.snps().order_by("start"):
+            variant_panel = make_variant_probe(al, variant, args.kmer, DB=DB)
+            for i, ref in enumerate(variant_panel.refs):
+                sys.stdout.write(
+                    ">ref-%s?var_name=%snum_alts=%i&ref=%s&enum=%i\n" %
+                    (variant_panel.variant.var_hash, variant.var_name[:100], len(
+                        variant_panel.alts), variant_panel.variant.reference.id, i))
+                sys.stdout.write("%s\n" % ref)
+            for i, a in enumerate(variant_panel.alts):
+                sys.stdout.write(">alt-%s?var_name=%s&enum=%i\n" %
+                                 (variant_panel.variant.var_hash, variant.var_name[:100], i))
+                sys.stdout.write("%s\n" % a)
